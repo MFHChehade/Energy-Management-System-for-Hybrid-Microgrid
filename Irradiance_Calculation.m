79dd8da6c3be3f2c6e3e38f032707156bdb04864
Mohamad Fares El Hajj Chehade <122603197+MFHChehade@users.noreply.github.com>
2023-01-16 13:41:32 +0200

    Add files via upload

---
+++ Irradiance_Calculation.m
@@ -0,0 +1,34 @@
+%%
+close all;
+clear all;
+clc;
+solar_rad = csvread('radiation_data.csv',1,0);
+
+%%
+DY_per_MO = [31,29,31,30,31,30,31,31,30,31,30,31];
+cum_days = cumsum(DY_per_MO);
+% solar_rad(730:750,:)                              % check
+for i = 745:8784
+    solar_rad(i,2) = solar_rad(i,2) + cum_days(solar_rad(i,1) - 1);
+end
+% solar_rad(730:750,:)                              % check
+
+irradiance_available_at_panels = zeros(366,24);
+
+for i = 1:8784
+    
+    %[s_radiation] = radiation_slope_surface (day, time, G_horizontal_radiation, solar_zenith);
+    day = solar_rad(i,2);
+    time = solar_rad(i,3);
+    solar_zenith = solar_rad(i,5);
+    G_horizontal_radiation = solar_rad(i,4);
+    
+    irradiance = radiation_slope_surface (day, time, G_horizontal_radiation, solar_zenith);
+    
+    irradiance_available_at_panels(day,time+1) = irradiance;
+    
+end
+
+%%
+csvwrite('solar_irradiance.csv', irradiance_available_at_panels)
+
